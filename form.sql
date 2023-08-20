8b35d94dfedf9c23363eca5974d85f98fea7617c
Gowtham021 <122508207+Gowtham021@users.noreply.github.com>
2023-08-20 21:18:15 +0530

    Update form.sql

--- form.sql
+++ form.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE contact_form (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fullName VARCHAR(255) NOT NULL,
+    phoneNumber VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    ipAddress VARCHAR(45) NOT NULL,
+    timestamp DATETIME NOT NULL
+);
